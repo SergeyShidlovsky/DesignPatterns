249a2a20cbe88e835290227f2e00403ccd0f0020
SergeyShidlovsky <shedlovskys@gmail.com>
2019-11-01 13:03:04 +0200

    Initial commit in the branch

--- src/com/company/creational/factory/buttons/HtmlButton.java
+++ src/com/company/creational/factory/buttons/HtmlButton.java
@@ -1,4 +1,14 @@
 package com.company.creational.factory.buttons;
 
-public class HtmlButton {
+public class HtmlButton implements Button {
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void onClick() {
+
+    }
 }
--- src/com/company/creational/singleton/Singleton.java
+++ src/com/company/creational/singleton/Singleton.java
@@ -1,5 +1,17 @@
-package com.company.creational;
+package com.company.creational.singleton;
 
-public class Test {
-    //Here will be pattern
+public class Singleton {
+    private static Singleton instance;
+    public String value;
+
+    private Singleton(String value){
+        this.value = value;
+    }
+
+    public static Singleton getInstace(String value){
+        if (instance == null){
+            instance = new Singleton(value);
+        }
+        return instance;
+    }
 }
