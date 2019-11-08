4fe2b619462d12361edb5d5e935539299ecf4163
SergeyShidlovsky <shedlovskys@gmail.com>
2019-11-08 19:46:38 +0200

    Final commit

--- src/com/company/creational/factory/Demo.java
+++ src/com/company/creational/factory/Demo.java
@@ -1,12 +1,27 @@
 package com.company.creational.factory;
 
 import com.company.creational.factory.factory.Dialog;
+import com.company.creational.factory.factory.HtmlDialog;
+import com.company.creational.factory.factory.WindowsDialog;
 
 public class Demo {
 
     private static Dialog dialog;
 
+    public static void main(String[] args) {
+        configure();
+        runBusinessLogic();
+    }
+
     static void configure(){
-        if(System){}
+        if(System.getProperty("os.name").equals("Windows 10")){
+            dialog = new WindowsDialog();
+        } else {
+            dialog = new HtmlDialog();
+        }
+    }
+
+    static void runBusinessLogic(){
+        dialog.renderWindow();
     }
 }
